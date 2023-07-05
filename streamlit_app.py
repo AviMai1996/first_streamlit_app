f9b62a95e58db96413ec727d57d40410a4ebf81a
AviMai1996 <138566313+AviMai1996@users.noreply.github.com>
2023-07-05 14:21:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,15 @@
 import streamlit
-streamlit.title('Hello World')
+streamlit.title('One Piece')
+
+streamlit.header('Crew')
+
+streamlit.header('Luffy')
+streamlit.header('Zoro')
+streamlit.header('Nami')
+streamlit.header('Robin')
+streamlit.header('Sanji')
+streamlit.header('Chopper')
+streamlit.header('Brook')
+streamlit.header('Ussop')
+streamlit.header('Franky')
+streamlit.header('Jimbei')
